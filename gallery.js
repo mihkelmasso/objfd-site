86d0cf63a1cca71b0cc6ee550b271e79279642d3
mihkelmasso <mihkelmasso@gmail.com>
2026-06-06 02:12:26 +0300

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,17 @@
+const gallery = document.getElementById("gallery");
+
+if (gallery && typeof PRODUCT_FOLDER !== "undefined" && typeof IMAGE_COUNT !== "undefined") {
+  for (let i = 1; i <= IMAGE_COUNT; i++) {
+    const num = String(i).padStart(2, "0");
+    const img = document.createElement("img");
+
+    img.src = `../images/${PRODUCT_FOLDER}/${num}.jpg`;
+
+    img.onerror = () => {
+      img.src = `../images/${PRODUCT_FOLDER}/${num}.png`;
+      img.onerror = () => img.remove();
+    };
+
+    gallery.appendChild(img);
+  }
+}
